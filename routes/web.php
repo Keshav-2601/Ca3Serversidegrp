6955251dd11611ec4bb3f3372f7b1a7aa69875e7
Keshav-2601 <keshavv857@gmail.com>
2024-04-22 20:44:55 +0100

    git 7 making login for guest as well

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/homepage',[HomePageController::class,"index"])->name('homepage');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
