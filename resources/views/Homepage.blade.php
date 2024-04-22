594a16ba90ea71b49b7d26556f20535fc0a4ad61
Keshav-2601 <keshavv857@gmail.com>
2024-04-22 20:15:51 +0100

    git 6 making Registaration and Login for application

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -3,7 +3,7 @@
         <tr>
             <th>ID</th>
             <th>Name</th>
-            <th>Description</th>
+            <!-- <th>Description</th> -->
             <!-- <th>Rating</th> -->
         </tr>
     </thead>
@@ -12,11 +12,12 @@
             <tr>
                 <td>{{ $destination->id }}</td>
                 <td>{{ $destination->name }}</td>
-                <td>{{ $destination->description }}</td>
+                <!-- <td>{{ $destination->description }}</td> -->
                 <!-- <td>{{ $destination->rating }}</td> -->
                 @foreach ($destination->images as $image)
                       <td><img src="{{ asset($image->image_path) }}" alt="Image for {{ $destination->name }}"></td>
                 @endforeach
+                <td><button>More_Info</button></td>
             </tr>
         @endforeach
     </tbody>
