1b1745bf27805f3265d35d72fc97c9cd95342d31
Keshav-2601 <keshavv857@gmail.com>
2024-05-04 15:09:22 +0100

    git 29 Completing edit page css

--- resources/views/homepage/edit_destination.blade.php
+++ resources/views/homepage/edit_destination.blade.php
@@ -1,6 +1,21 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section('content')
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        
+        <!-- Scripts -->
+        @vite(['resources/css/editpage.css', 'resources/js/app.js'])
+    </head>
+    <body>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -50,4 +65,5 @@
             </div>
         </div>
     </div>
-@endsection
+</body>
+
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/mainpage.css',
+                'resources/css/editpage.css',
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
