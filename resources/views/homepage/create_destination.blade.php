1625d4474e6170c42242194ee118f9032786c7ed
Keshav-2601 <keshavv857@gmail.com>
2024-05-04 15:44:34 +0100

    git 31 Completing the Add destination css

--- resources/views/homepage/create_destination.blade.php
+++ resources/views/homepage/create_destination.blade.php
@@ -1,6 +1,22 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section('content')
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        
+        <!-- Scripts -->
+        @vite(['resources/css/createdestinationpage.css', 'resources/js/app.js'])
+    </head>
+
+<body>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -22,7 +38,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="rating">Rating</label>
-                                <input class="form-control" id="description" name="rating"></input>
+                                <input  type='text' class="form-control" id="rating" name="rating"></input>
                             </div>
 
                             <div class="form-group">
@@ -82,4 +98,5 @@
             document.getElementById('hotels-container').insertAdjacentHTML('beforeend', html);
         });
     </script>
-@endsection
+
+    </body>
